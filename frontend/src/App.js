97243f6c404693cb1a794844cea7e50c70f8e3ee
jannathsyeda <jannathsyeda7@gmail.com>
2022-11-05 01:46:27 +0600

    router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import Footers from './Components/Footers'
 import Header from './Components/Header'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter , Routes,Route } from "react-router-dom";
+
 import HeaderScreens from './Screens/HeaderScreens'
+import ProductScreen from './Screens/ProductScreen';
 function App() {
   return (
     <>
       <Header />
       <main className="my-2">
         <Container>
-       <HeaderScreens/>
+          <BrowserRouter>
+          <Routes>
+                        <Route path='/' element={<HeaderScreens/>} />
+
+            <Route path='/product/:id' element={<ProductScreen/>} />
+   
+       </Routes>
+       </BrowserRouter>
         </Container>
       </main>
       <Footers />
