319406bacc5f9faad401432eee24cd2cd510ec52
jannathsyeda <jannathsyeda7@gmail.com>
2022-11-05 22:17:40 +0600

    router added

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -2,18 +2,34 @@ import React from 'react'
 import {
     Container ,Nav ,Navbar  
 } from 'react-bootstrap'
+import {  BrowserRouter} from 'react-router-dom'
+import {LinkContainer} from "react-router-bootstrap"
 const Header = () => {
   return <header>
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href='/'>E-Shop</Navbar.Brand>
+        <BrowserRouter>
+        <LinkContainer to="/">
+        <Navbar.Brand href='/'>E-shop</Navbar.Brand>
+          </LinkContainer>
+          </BrowserRouter>
+       
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='ms-auto'>
-            <Nav.Link href='/cart'>
+          <BrowserRouter>
+          <LinkContainer to="/cart">
+       
+               <Nav.Link  >
               <i className='fas fa-shopping-cart'></i>Cart
               </Nav.Link>
-            <Nav.Link href='/login'><i className='fas fa-user'></i>Sign In</Nav.Link>
+              </LinkContainer>
+              </BrowserRouter>
+              <BrowserRouter>
+          <LinkContainer to="/login">
+            <Nav.Link ><i className='fas fa-user'></i>Sign In</Nav.Link>
+            </LinkContainer>
+            </BrowserRouter>
           
           </Nav>
         </Navbar.Collapse>
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import products from '../products.js'
 
 const ProductScreen = () => {
+  const {id}=useParams()
+  const product=products.find((p)=> p._id===id)
   return (
-    <div>ProductScreen</div>
+   
   )
 }
 
