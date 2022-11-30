6fdd347d82b58376e1381466629df0a9d01888f8
tahmina <fahima.jannath@sjinnovation.com>
2022-11-30 16:58:52 +0600

    products listing redux state done

--- frontend/src/Screens/HeaderScreens.js
+++ frontend/src/Screens/HeaderScreens.js
@@ -1,20 +1,31 @@
-import {React, useEffect,useState}from 'react'
+import {React, useEffect}from 'react'
 import {Row,Col} from 'react-bootstrap'
 import Product from '../Components/Product'
 import { useDispatch, useSelector } from 'react-redux'
 import {listProduct} from '../actions/productAction'
+import Loader from '../Components/Loader.js'
+import Message from '../Components/Message'
 const HeaderScreens = () => {
-
+const productsList=useSelector((state)=>state.productList)
+const {loading,products,error}=productsList
 const dispatch=useDispatch()
 
 useEffect(()=>{
     dispatch(listProduct())
 },[dispatch])
-const products=[]
+
 
   return (
 <>
-<h1>Latest Products</h1>
+<h1>Latest Products</h1>{loading?( <Loader/> ):error?(<Message variant={'danger'}>{error}</Message>):(<Row>
+    {
+        products.map((product)=>(
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>
+            </Col>
+        ))
+    }
+</Row>)}
 <Row>
     {
         products.map((product)=>(
