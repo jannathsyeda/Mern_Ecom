790471e6fcc0b2d9480cba7111e43a012e90e1ec
jannathsyeda <jannathsyeda7@gmail.com>
2023-11-22 16:25:56 +0600

    meta update and done the prject

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,10 @@ import mongoose from 'mongoose'
 
 const connectDB=async()=>{
     try{
-        const conn= await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ku9hx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
-            useUnifiedTopology:true,
+        // const conn= await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ku9hx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+         const conn= await mongoose.connect(process.env.MONGO_URI,{
+   
+        useUnifiedTopology:true,
             useNewUrlParser: true,
             
         })
--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ app.use(express.json())
 // }
 
 
-app.get('/',(req,res)=>{
-    res.send("api is running")
-})
+
 app.use('/api/products',RouterProduct )
 app.use('/api/users',RouterUsers )
 app.use('/api/orders',RouterOrder )
@@ -37,6 +35,20 @@ app.use('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 
 app.use(notFound)
